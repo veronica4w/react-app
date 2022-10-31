b7519afd841650835dec50410a36e74bd2323f2f
Ashwin20102000 <jkashwin2@gmail.com>
2022-10-31 17:45:00 +0530

    configs update

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 import { configInterface } from "../intefaces/config.interface";
 import { Config } from "./config";
 const configValues:configInterface = {
-    jsonApi: process.env.REACT_APP_URL||''
+    jsonApi: process.env.REACT_APP_URL||'https://jsonplaceholder.typicode.com'
 }
 export const configs: Config = new Config(configValues);
 export default configValues;
